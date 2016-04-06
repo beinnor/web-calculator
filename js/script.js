f8616cfcf6ba2891b903ee8c0d5a3701b7548b96
= <ronnie.bjorkelund@gmail.com>
2016-04-06 02:55:54 +0200

    CE function working

--- js/script.js
+++ js/script.js
@@ -1,85 +1,146 @@
-
-$(document).ready(function(){
-
-
-    var total = 0;
-    var firstNumber = "";
-    var secondNumber = "";
-    var operator = "";
-    var screen = $("#screen");
-    screen.text(0);
+$(document).ready(function() {
 
 
-    //Add your .click() here!
-    $("#numbers a").not("#clear, #clearall").click(function() {
+  var total = 0;
+  var firstNumber = "";
+  var secondNumber = "";
+  var operator = "";
+  var screen = $("#screen");
+  screen.text(0);
+
+  var firstNumberLog = $("#first");
+  var secondNumberLog = $("#second");
+  var totalLog = $("#total");
+  var opLog = $("#op");
+
+  firstNumberLog.text("firstNumber: " + firstNumber);
+  secondNumberLog.text("secondNumber: " + secondNumber);
+  totalLog.text("Total: " + total);
+  opLog.text("Total: " + operator);
 
 
-        firstNumber += $(this).text();
+  //Add your .click() here!
+  $("#numbers a").not("#clear, #clearall").click(function() {
 
 
-
-        if (firstNumber.length <= 9) {
-            screen.text(firstNumber);
-        } else {
-            screen.text("Error");
-            firstNumber = "";
-        }
-
-
-    });
-
-
-    $(".operators").click(function() {
-
-
-        operator = $(this).text();
-
-        secondNumber = firstNumber;
-        firstNumber = "";
-
-    });
-
-    $("#equals").click(function() {
+    firstNumber += $(this).text();
 
 
 
-        switch (operator) {
-            case "+":
-                total = (+secondNumber) + (+firstNumber);
-                break;
-            case "-":
-                total = (+secondNumber) - (+firstNumber);
-                break;
-            case "*":
-                total = (+secondNumber) * (+firstNumber);
-                break;
-            case "/":
-                total = (+secondNumber) / (+firstNumber);
-                break;
-        }
+    if (firstNumber.length <= 9) {
+      screen.text(firstNumber);
+    } else {
+      screen.text("Error");
+      firstNumber = "";
+    }
 
-        screen.text(total.toString());
-        firstNumber = total.toString();
+    firstNumberLog.text("firstNumber: " + firstNumber);
+    secondNumberLog.text("secondNumber: " + secondNumber);
+    totalLog.text("Total: " + total);
+    opLog.text("Total: " + operator);
 
-    });
+  });
 
 
-    $("#clear").click(function() {
+  $(".operators").click(function() {
 
-        firstNumber = "";
-        screen.text(total.toString());
 
-    });
+    operator = $(this).text();
+    secondNumber = firstNumber;
+    firstNumber = "";
 
-    $("#clearall").click(function() {
+    firstNumberLog.text("firstNumber: " + firstNumber);
+    secondNumberLog.text("secondNumber: " + secondNumber);
+    totalLog.text("Total: " + total);
+    opLog.text("Total: " + operator);
 
-        secondNumber = "";
-        firstNumber = "";
-        total = 0;
-        operator = "";
-        screen.text("0");
+  });
 
-    });
+  $("#equals").click(function() {
+
+
+    if (secondNumber !== "") {
+      switch (operator) {
+        case "+":
+          total = (+secondNumber) + (+firstNumber);
+          break;
+        case "-":
+          total = (+secondNumber) - (+firstNumber);
+          break;
+        case "*":
+          total = (+secondNumber) * (+firstNumber);
+          break;
+        case "/":
+          total = (+secondNumber) / (+firstNumber);
+          break;
+      }
+    } else {
+      switch (operator) {
+        case "+":
+          total = (+total) + (+firstNumber);
+          break;
+        case "-":
+          total = (+total) - (+firstNumber);
+          break;
+        case "*":
+          total = (+total) * (+firstNumber);
+          break;
+        case "/":
+          total = (+total) / (+firstNumber);
+          break;
+      }
+    }
+
+    screen.text(total);
+    firstNumber = "";
+
+    firstNumberLog.text("firstNumber: " + firstNumber);
+    secondNumberLog.text("secondNumber: " + secondNumber);
+    totalLog.text("Total: " + total);
+    opLog.text("Total: " + operator);
+
+  });
+
+
+  $("#clear").click(function() {
+
+    firstNumber = "";
+
+    if (secondNumber === "") {
+      secondNumber = "";
+      total = 0;
+      screen.text("0");
+
+    } else if (secondNumber !== "" && total !== 0) {
+      secondNumber = "";
+      total = 0;
+      screen.text("0");
+    } else {
+      screen.text(total.toString());
+    }
+
+
+    firstNumberLog.text("firstNumber: " + firstNumber);
+    secondNumberLog.text("secondNumber: " + secondNumber);
+    totalLog.text("Total: " + total);
+    opLog.text("Total: " + operator);
+
+  });
+
+  $("#clearall").click(function() {
+
+    secondNumber = "";
+    firstNumber = "";
+    total = 0;
+    operator = "";
+    screen.text("0");
+
+    firstNumberLog.text("firstNumber: " + firstNumber);
+    secondNumberLog.text("secondNumber: " + secondNumber);
+    totalLog.text("Total: " + total);
+    opLog.text("Total: " + operator);
+
+  });
 
 
 });
